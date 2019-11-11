96485f29ab8c329093ffa53c4baaf771add6d4f5
Terence Sun <tianruisun@gmail.com>
2019-11-11 22:11:16 +1100

    add 1 python code

---
+++ capstone_project.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Coursera Capstone Project
+# 
+# Description: This notebook is used to complete my first week assignment for Coursera Capstone
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
